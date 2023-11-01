da2cd194ab4e780ddaeb9f85d794f4eacc8286c6
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-11-01 11:01:59 +0000

    more slideshow stuff

--- client/js/customScripts.js
+++ client/js/customScripts.js
@@ -1,12 +1,12 @@
 // Custom made scripts (not from template)
 
-let pslide1, pslide2, pslide3, pslide4, pslide5, pslide6 = 0;
-showSlide(pslide1)
-showSlide(pslide2)
-showSlide(pslide3)
-showSlide(pslide4)
-showSlide(pslide5)
-showSlide(pslide6)
+let pslide1 = 0;
+let pslide2 = 0;
+let pslide3 = 0;
+let pslide4 = 0;
+let pslide5 = 0;
+let pslide6 = 0;
+
 
 slideDict = {
     "p1": ["pslide1", pslide1],
@@ -17,23 +17,35 @@ slideDict = {
     "p6": ["pslide6", pslide6],
 }
 
-
-
 function showSlide(x,type) {
+    area = slideDict[type][0]
     let slides = document.getElementsByClassName(area)
     if (x >= slides.length) {
         slideDict[type][1] = 0
     }
     if (x < 0) {
         slideDict[type][1] = slides.length - 1
+        x = slideDict[type][1]
     }
     for (i=0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideDict[type][1]].style.display = 'Block'
+    slides[x].style.display = 'Block'
+    slides[x].style.opacity = '1'
 }
 
 function changeSlide(x, type) {
     let index = slideDict[type][1]
     showSlide(index += x, type)
 }
+
+function load() {
+    showSlide(pslide1, "p1")
+    // showSlide(pslide2)
+    // showSlide(pslide3)
+    // showSlide(pslide4)
+    // showSlide(pslide5)
+    // showSlide(pslide6)
+}
+
+document.addEventListener('DOMContentLoaded', load)
